53f8740b7eda2d0ffde633529bd862f953b4221d
raysa <raysadilia@gmail.com>
2019-04-30 20:40:42 +0700

    python file lain

---
+++ purwadhika.py
@@ -0,0 +1,53 @@
+# print ('Hello World!!!')
+# print ('Halo')
+# nama = 'Andi'
+# usia = 22
+# print (nama)
+# usia_1 = '22'
+# usia_2 = '32'
+# print (usia_1)
+# print (usia_2)
+# jomblo = True
+# print (jomblo)
+# nama = 'Andi'
+# jomblo = True
+# # test aja
+# nama = 'Andi';
+# usia = 22;
+# jomblo = True;
+# contoh = 22.4;
+# print (type(nama));
+# print(type(usia));
+# print(type(jomblo));
+# print (type(contoh));
+
+# nama = input("Whats your name? : ");
+# print(nama);
+
+# print(5 * '4')
+
+# contoh baru text (String)
+# j = 0
+
+nama = input('Nama Kamu? : ');
+umur = input('Umur Kamu? : ');
+gender = input('Kelamin Kamu? : ');
+pekerjaan = input('Pekerjaan Kamu? : ');
+
+print(nama);
+print(umur);
+print(gender);
+print(pekerjaan);
+
+# Contoh Numbers and Aritmatic Operations
+# usiaAndi = 40;
+# usiaBudi = 20;
+
+# print (usiaAndi ** usiaBudi)
+# print (usiaAndi + usiaBudi)
+
+# # Contoh import Math
+# import math
+# print (math.pi)
+
+
